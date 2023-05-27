1cc42881d43833425db625ebf8590b9b26d7a262
Ice Cubenetix <macandcheese808@icloud.com>
2023-05-27 21:38:06 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,24 @@
+console.log("Welcome!");
+console.log('Made By shinobiplug"');
+
+var loader = document.getElementById('preloader');
+
+window.addEventListener('load', function() {
+    loader.style.display = 'none'
+})
+
+function Website() {
+    window.open('https://shinobiplug.netlify.app', '_blank')
+}
+
+function KingPods() {
+    window.open('https://kingpods.netlify.app', '_blank')
+}
+
+function Instagram() {
+    window.open('https://instagram.com/shinobiplug_', '_blank')
+}
+
+function TikTok() {
+    window.open('https://tiktok.com/@shinobiplug_', '_blank')
+}
